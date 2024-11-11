9e790fef88dc9214933da62596ede90e2044d859
chrisj0110 <45946825+chrisj0110@users.noreply.github.com>
2024-11-11 16:16:49 -0600

    bubbletea structure

--- main.go
+++ main.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import(
+    "fmt"
+    "os"
+
+    tea "github.com/charmbracelet/bubbletea"
+)
+
+type model struct {
+    testing string
+}
+
+func initialModel() model {
+    return model{
+        testing: "testing",
+    }
+}
+
+func (m model) Init() tea.Cmd {
+    return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+    switch msg := msg.(type) {
+    case tea.KeyMsg:
+        switch msg.String() {
+        case "ctrl+c", "q":
+            return m, tea.Quit
+        }
+    }
+
+    return m, nil
+}
+
+func (m model) View() string {
+    s := "Press q to exit"
+    return s
+}
 
 func main() {
-    fmt.Println("Hello, world")
+    p := tea.NewProgram(initialModel())
+    if _, err := p.Run(); err != nil {
+        fmt.Printf("There has been an error: %v", err)
+        os.Exit(1)
+    }
 }
