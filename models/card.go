779958d4eb87a6adf36b04c40db3254b299cd2a4
chrisj0110 <45946825+chrisj0110@users.noreply.github.com>
2024-11-16 13:52:11 -0600

    card background; fix Ace

--- models/card.go
+++ models/card.go
@@ -1,12 +1,22 @@
 package models
 
+import "github.com/charmbracelet/lipgloss"
+
 type Card struct {
     Rank CardRank
     Suit CardSuit
 }
 
+var redCard = lipgloss.NewStyle().Foreground(lipgloss.Color("9")).Background(lipgloss.Color("15"))
+var blackCard = lipgloss.NewStyle().Foreground(lipgloss.Color("0")).Background(lipgloss.Color("15"))
+
 func (c Card) Render() string {
-    return c.Rank.String() + c.Suit.String()
+    content := " " + c.Rank.String() + c.Suit.String() + " "
+    if c.Suit.isRed() {
+        return redCard.Render(content) 
+    } else {
+        return blackCard.Render(content)
+    }
 }
 
 type CardRank int
@@ -28,7 +38,7 @@ const (
 )
 
 func (r CardRank) String() string {
-    return [...]string{"A", "1", "2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K"}[r-1]
+    return [...]string{"A", "2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K"}[r-1]
 }
 
 type CardSuit int
@@ -42,4 +52,11 @@ const (
 
 func (s CardSuit) String() string {
     return [...]string{"♣️", "♦️", "❤️", "♠️"}[s]
+}
+
+func (s CardSuit) isRed() bool {
+    if s == 0 || s == 3 {
+        return false
+    }
+    return true
 }
